a14906e1a6778daad02b6f070451c1889d8ba42c
saiteja <kondasaitej@protonmail.com>
2020-10-05 14:03:10 +0530

    added localStorage

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchPosts } from '../store/actions/postAction';
 
 export const Post = props => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
   const { id } = props.match.params;
   const { postReducer } = useSelector(state => state);
-  const post = postReducer.posts.filter(p => {
-    return p.id === id;
-  });
-  console.log(post);
+// eslint-disable-next-line
+  let selectedPost = postReducer.posts.find(post => post.id == id);
+  const [post,] = useState(selectedPost);
+
   return (
     <div className="container">
       <div className="row">
         <h1>{post.title}</h1>
+        <h6>{post.postedOn}</h6>
+        <div>
+          <h6>Tags</h6>
+          {post.tags?.map(tag => (
+            <button>{tag.name}</button>
+          ))}
+        </div>
+        <hr />
+        <div>
+          <h6>comments</h6>
+          {post.comments?.map(comment => (
+            <div>
+              <h6>{comment.commentor}</h6>
+              <p>{comment.comment}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { createStore, applyMiddleware } from "redux";
-// import postReducer from "./store/reducers/postReducer"; 
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { Provider } from "react-redux";
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+// import postReducer from "./store/reducers/postReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import rootReducer from './store/rootReducer';
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle'
+const presistedState = loadState();
+const store = createStore(rootReducer, presistedState, composeWithDevTools(applyMiddleware(logger, thunk)));
 
-const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger, thunk)) );
+store.subscribe(throttle(() => {
+  saveState(store.getState());
+}, 50000));
+
 
 ReactDOM.render(
   <Provider store={store}>
@@ -18,7 +25,7 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
